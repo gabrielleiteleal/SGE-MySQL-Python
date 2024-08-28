6fd049b275a0cefa0aa1cf6c3274876ea0f8f26d
Gabriel Leal <94023072+gabrielleiteleal@users.noreply.github.com>
2024-08-28 11:05:23 -0300

    Update

--- Front/JS/functions.js
+++ Front/JS/functions.js
@@ -4,26 +4,26 @@ window.onload = function(){
 
     btn.addEventListener("click", function(e) {
 
-        e.preventDefault();
+        //e.preventDefault();
 
         const nome = document.querySelector("#nome");
         const idade = document.querySelector("#idade");
-        const endereco = document.querySelector("#endereco");
-        const email = document.querySelector("#email");
-        const telefone = document.querySelector("#tel");
+        //const endereco = document.querySelector("#endereco");
+        //const email = document.querySelector("#email");
+        //const telefone = document.querySelector("#tel");
         
         const value_nome = nome.value;
         const value_idade = idade.value;
-        const value_endereco = endereco.value;
-        const value_email = email.value;
-        const value_telefone = telefone.value;
+        //const value_endereco = endereco.value;
+        //const value_email = email.value;
+        //const value_telefone = telefone.value;
 
 
         console.log(value_nome);
         console.log(value_idade);
-        console.log(value_endereco);
-        console.log(value_email);
-        console.log(value_telefone);
+        //console.log(value_endereco);
+        //console.log(value_email);
+        //console.log(value_telefone);
 
     });
 
